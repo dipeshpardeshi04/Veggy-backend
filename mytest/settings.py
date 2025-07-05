a3bea9ce9c002b9ac965692d928c8268372f59c3
dipeshpardeshi04 <pardeshidipesh9050@gmail.com>
2025-07-05 16:39:39 +0530

    allow host

--- mytest/settings.py
+++ mytest/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'mytest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-    
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
